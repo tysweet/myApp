969f29b76b79c0d70f3f6f8948374c2216bb4f1f
Ty Sweet <tysweet77@gmail.com>
2018-04-11 15:19:56 -0500

    more and more

--- src/App.js
+++ src/App.js
@@ -10,6 +10,17 @@ import Bday from './components/bday.js';
 import Because from './components/because.js';
 import BlackAdder from './components/blackadder.js';
 import Bucket from './components/bucket.js';
+import CanIUse from './components/canIUse.js';
+import Chainsaw from './components/chainsaw.js';
+import Cocksplat from './components/cocksplat.js';
+import Cool from './components/cool.js';
+import Cup from './components/cup.js';
+import Dalton from './components/dalton.js';
+import Deraadt from './components/deraadt.js';
+import Diabetes from './components/diabetes.js';
+// import DoSomething from './components/doSomething.js';
+import Everyone from './components/everyone.js';
+import Everything from './components/everything.js';
 import Off from './components/off.js';
 import './App.css';
 
@@ -36,6 +47,17 @@ class App extends Component {
         <Because /><Divider />
         <BlackAdder /><Divider />
         <Bucket /><Divider />
+        <CanIUse /><Divider />
+        <Chainsaw /><Divider />
+        <Cocksplat /><Divider />
+        <Cool /><Divider />
+        <Cup /><Divider />
+        <Dalton /><Divider />
+        <Deraadt /><Divider />
+        <Diabetes /><Divider />
+        {/* <DoSomething /><Divider /> */}
+        <Everyone /><Divider />
+        <Everything /><Divider />
         <Off /><Divider />
       </div>
     );
---
+++ src/components/doSomething.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import '../App.css';
+
+class DoSomething extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      do: 'Run',
+      something: 'numbers',
+      from: 'Mom',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    const doNow = this.state.do;
+    const something = this.state.something;
+    const from = this.state.from;
+    fetch(`http://foaas.com/dosomething/${doNow}/${something}/${from}`, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    })
+    .then((results) => {
+        return results.json();
+    })
+    .then((json) => {
+      let message = json.message;
+      let subtitle = json.subtitle;
+      this.setState({message: message});
+      this.setState({subtitle: subtitle});
+    })
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.type === 'input' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({[name]: value});
+  }
+
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const newDo = this.state.do;
+    const newSomething = this.state.something;
+    const newFrom = this.state.from;
+    this.setState({name: newDo});
+    this.setState({name: newSomething});
+    this.setState({from: newFrom});
+    this.componentDidMount();
+  }
+
+
+
+  render() {
+    return (
+      <div style={{marginTop: 20}}>
+        <form>
+          <label style={{marginLeft: 38}}>
+            Do:
+            <input name="name" className="name" type="input" style={{margin: 5}}
+              onChange={this.handleChange} />
+          </label>
+          <br />
+          <label style={{marginLeft: 12}}>
+            Something:
+            <input name="something" className="something" type="input" style={{margin: 5}}
+              onChange={this.handleChange} />
+          </label>
+          <br />
+          <label style={{marginLeft: 42}}>
+            From:
+            <input name="from" className="from" type="input" style={{margin: 5}}
+              onChange={this.handleChange} />
+          </label><br />
+          <RaisedButton label="Submit" style={{marginLeft: 110, marginTop: 10}} onClick={this.handleSubmit} value="Submit" />
+        </form>
+        <div className="container" style={{margin: 10}}>
+          <h1>{this.state.message}</h1>
+          <h3>{this.state.subtitle}</h3>
+        </div>
+      </div>
+    );
+  }
+}
+export default DoSomething;
