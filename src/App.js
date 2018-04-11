e92b25ef580d323957c2320da7f0509bdbaf5a71
Ty Sweet <tysweet77@gmail.com>
2018-04-11 14:33:15 -0500

    coming along

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
-// import Name from './Name.js';
-// import From from './From.js';
-import RaisedButton from 'material-ui/RaisedButton';
+import Divider from 'material-ui/Divider';
+import Anyway from './components/anyway.js';
+import Asshole from './components/asshole.js';
+import Awesome from './components/awesome.js';
+import Back from './components/back.js';
+import Bag from './components/bag.js';
+import Ballmer from './components/ballmer.js';
+import Bday from './components/bday.js';
+import Because from './components/because.js';
+import BlackAdder from './components/blackadder.js';
+import Bucket from './components/bucket.js';
+import Off from './components/off.js';
 import './App.css';
 
 
@@ -9,92 +18,25 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: '',
-      name: 'Bob',
-      from: 'Mom',
-      company: 'ABC Inc.',
-      tool: 'hammer',
-      do: 'Smash',
-      something: 'window',
-      reference: 'FOAAS 3:16',
-      noun: 'taco',
-      reaction: 'cry',
-      behavior: 'smiling',
-      thing: 'computer',
-      language: 'Swahili',
-      message: '',
-      subtitle: '',
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  componentDidMount() {
-    const name = this.state.name;
-    const from = this.state.from;
-    fetch(`http://foaas.com/off/${name}/${from}`, {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-    })
-    .then((results) => {
-        return results.json();
-    })
-    .then((json) => {
-      let message = json.message;
-      let subtitle = json.subtitle;
-      this.setState({message: message});
-      this.setState({subtitle: subtitle});
-    })
-  }
-
-  handleChange(event) {
-    const target = event.target;
-    const value = target.type === 'input' ? target.checked : target.value;
-    const name = target.name;
-    this.setState({[name]: value});
-  }
-
-
-  handleSubmit(event) {
-    event.preventDefault();
-    const newName = this.state.name;
-    const newFrom = this.state.from;
-    this.setState({name: newName});
-    this.setState({from: newFrom});
-    this.componentDidMount();
-  }
-
-
+  };
 
   render() {
     return (
       <div>
-        <form>
-          <label>
-            First Name:
-            <input name="name" className="name" type="input" style={{margin: 5}}
-              onChange={this.handleChange} />
-          </label>
-          <br />
-          <label style={{marginLeft: 42}}>
-            From:
-            <input name="from" className="from" type="input" style={{margin: 5}}
-              onChange={this.handleChange} />
-          </label><br />
-          {/* <Name
-              name={this.state.value}
-            />
-            <From
-            from={this.state.value}
-          /> */}
-          <RaisedButton label="Submit" style={{marginLeft: 110, marginTop: 10}} onClick={this.handleSubmit} value="Submit" />
-        </form>
-        <div className="container" style={{margin: 10}}>
-          <h1>{this.state.message}</h1>
-          <h3>{this.state.subtitle}</h3>
-        </div>
+        <header><h1>FOAAS</h1></header>
+        <Divider />
+        <Anyway /><Divider />
+        <Asshole /><Divider />
+        <Awesome /><Divider />
+        <Back /><Divider />
+        <Bag /><Divider />
+        <Ballmer /><Divider />
+        <Bday /><Divider />
+        <Because /><Divider />
+        <BlackAdder /><Divider />
+        <Bucket /><Divider />
+        <Off /><Divider />
       </div>
     );
   }
